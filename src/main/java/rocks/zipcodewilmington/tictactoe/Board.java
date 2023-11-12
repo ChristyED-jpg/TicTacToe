abce61228b9332d6a787ff04eb2eb87cb699929a
Christy <christy@zipcoders-mbp-6.mynetworksettings.com>
2023-11-11 21:48:05 -0500

    trying to fix my method

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,25 +4,79 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
 
     public Board(Character[][] matrix) {
-         this.matrix = matrix;
+        this.matrix = matrix;
     }
+    //[0][0], [0][1], [0][2]
+    //[1][0], [2][0], [1][2]
+    //[2][0], [2][1], [2][2]
+
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        return checkRow('X') || checkColumn('X') || checkDiagonal('x');
     }
 
-    public Boolean isInFavorOfO() {
-        return null;
+    public boolean checkRow(Character letter) {
+
+        for (int i = 0; i < i; i++) {
+            if (this.matrix[i][0] == letter && this.matrix[i][1] == letter && this.matrix[i][2] == letter) {
+                // when i = 0
+                // [0][0] and [0][1] and [0][2]
+
+                return true;
+            }
+        }
+        return false;
     }
 
-    public Boolean isTie() {
-        return null;
+    public boolean checkColumn(Character letter) {
+        for (int i = 0; i < 3; i++) {
+            if (this.matrix[0][1] == letter && this.matrix[1][i] == letter && this.matrix[2][i] == letter) {
+                // i = 1
+                // [0][1] -> [1][1] -> [2][1]
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    public String getWinner() {
-        return null;
-    }
+    public boolean checkDiagonal(Character letter) {
+        for (int i = 0; i < 3; i++) {
+            if (this.matrix[2][i] == letter && this.matrix[2][1] == letter && this.matrix[2][2] == letter) {
+                return true;
+            }
+            // i = 2
+            // [2][0], [2][1], [2][2]
 
-}
+            return false;
+        }
+
+
+
+        public Boolean isInFavorOfO() {
+
+            return checkRow('0') || checkColumn('0') || checkDiagonal('0');
+        }
+
+
+        public Boolean isTie() {
+       if (isInFavorOfO() == isInFavorOfX()){
+        /// FALSE ++ FALSE  -> TIE
+        // both return false because there is winner
+            return false;
+        }
+
+        public String getWinner () {
+            if (isInFavorOfX()) {
+                return "O";
+            }else if (isInFavorOfX()){
+                return "X";
+            }else return "";
+
+
+        }
+
