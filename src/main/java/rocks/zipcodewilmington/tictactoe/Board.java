055d619c336cb36db952970e65af74001393d467
Christy <christy@Zipcoders-MacBook-Pro-90.local>
2023-12-08 12:58:47 -0500

    finish

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -14,14 +14,11 @@ public class Board {
     //[2][0], [2][1], [2][2]
 
 
-    public Boolean isInFavorOfX() {
 
-        return checkRow('X') || checkColumn('X') || checkDiagonal('x');
-    }
 
     public boolean checkRow(Character letter) {
 
-        for (int i = 0; i < i; i++) {
+        for (int i = 0; i < 3; i++) {
             if (this.matrix[i][0] == letter && this.matrix[i][1] == letter && this.matrix[i][2] == letter) {
                 // when i = 0
                 // [0][0] and [0][1] and [0][2]
@@ -34,7 +31,7 @@ public class Board {
 
     public boolean checkColumn(Character letter) {
         for (int i = 0; i < 3; i++) {
-            if (this.matrix[0][1] == letter && this.matrix[1][i] == letter && this.matrix[2][i] == letter) {
+            if (this.matrix[0][i] == letter && this.matrix[1][i] == letter && this.matrix[2][i] == letter) {
                 // i = 1
                 // [0][1] -> [1][1] -> [2][1]
                 return true;
@@ -45,20 +42,32 @@ public class Board {
     }
 
     public boolean checkDiagonal(Character letter) {
-        for (int i = 0; i < 3; i++) {
-            if (this.matrix[2][i] == letter && this.matrix[2][1] == letter && this.matrix[2][2] == letter) {
-                return true;
-            }
-            // i = 2
-            // [2][0], [2][1], [2][2]
+        if (this.matrix[0][0] == letter && this.matrix[1][1] == letter && this.matrix[2][2] == letter) {
+            return true;
+        } else  if (this.matrix[0][2] == letter && this.matrix[1][1] == letter && this.matrix[2][0] == letter){
+            return true;}
+        else{
+
         }
         return false;
     }
+    //
+    //   [i,j] [0,1] [0,2]
+    //   [1,0] [1,1] [1,2]
+    //   [2,0] [2,1] [2,2]
+    //
 
         public Boolean isInFavorOfO () {
 
-            return checkRow('0') || checkColumn('0') || checkDiagonal('0');
+            return checkRow('O') || checkColumn('O') || checkDiagonal('O');
+
         }
+
+
+    public Boolean isInFavorOfX() {
+
+        return checkRow('X') || checkColumn('X') || checkDiagonal('X');
+    }
 
 
         public Boolean isTie () {
@@ -69,7 +78,7 @@ public class Board {
         }
 
         public String getWinner () {
-            if (isInFavorOfX()) {
+            if (isInFavorOfO()) {
                 return "O";
             } else if (isInFavorOfX()) {
                 return "X";
